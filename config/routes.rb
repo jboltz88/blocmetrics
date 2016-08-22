ab2fc38788a621689db155d9cce6cfe4f95d9596
Justin Boltz <boltz.justin@gmail.com>
2016-08-22 13:55:21 -0500

    Complete development email confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
   get 'welcome/show'
   root to: 'welcome#show'
 end
