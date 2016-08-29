7e0cd42cddb557343d2164af7174724ad5560744
Justin Boltz <boltz.justin@gmail.com>
2016-08-29 14:52:23 -0500

    Fix routes.rb typo

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   root to: 'welcome#show'
 
   namespace :api, defaults: { format: :json } do
-      resources :events, only: [:create]
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
   end
 end
